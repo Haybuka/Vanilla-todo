42166b28fda0ca793ec41e6a1e929acfe8fd0cb2
haybuka <paschal.nduleo@gmail.com>
2023-04-21 01:33:35 +0100

    Included checkbox toggler

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const listItems = document.querySelector(".todo-items");
 const form = document.querySelector("form");
 
 let todoItemsArray = [];
-// localStorage.setItem("todos", JSON.stringify(todoItemsArray));
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   let value = this[0].value;
@@ -22,28 +22,39 @@ const createElements = (item) => {
   const button = document.createElement("button");
   const listItem = document.createElement("li");
 
+  button.setAttribute("type", "button");
+
+  console.dir(checkbox);
   todoText.innerHTML = item.value;
   button.innerHTML = "Delete";
   checkbox.setAttribute("type", "checkbox");
   textContainer.append(checkbox, todoText);
-  //   console.log(textContainer);
 
   listItem.setAttribute("class", "todo-item");
+  listItem.setAttribute("id", item.id);
   listItem.append(textContainer, button);
   listItems.prepend(listItem);
 };
 
 const appendTodo = () => {
-  console.dir(listItems);
   if (listItems.childElementCount > 0) {
     listItems?.removeChild(listItems.querySelector(".todo-item"));
   }
   JSON.parse(localStorage.getItem("todos"))?.forEach((item) => {
-    console.log("item called");
     const createdelement = createElements(item);
+  });
+
+  listItems.querySelectorAll("input[type='checkbox']").forEach((item) => {
+    item.addEventListener("click", function () {
+      console.dir(this.nextElementSibling);
+      if (!this.nextElementSibling.className.includes("checked")) {
+        this.nextElementSibling.classList.add("checked");
+      } else {
+        this.nextElementSibling.classList.remove("checked");
+      }
+      // console.dir(this.nextElementSibling.classList.add('checked'));
+    });
   });
 };
 
-// console.log(todoItemsArray);
-
-window.addEventListener('load',appendTodo())
+window.addEventListener("load", appendTodo());
